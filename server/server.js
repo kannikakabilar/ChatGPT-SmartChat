c4ed0f3a1278af0b63621f38f5f6f87cf033858f
Kannika Kabilar <57203769+kannikakabilar@users.noreply.github.com>
2023-02-07 19:48:10 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
+// establish configuration with openai
 dotenv.config();
-
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
 
+// use express framework to send post request and get request to communicate with the API
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -21,6 +21,7 @@ app.get('/', async (req, res) => {
     })
 });
 
+// settings of ChatGPT's AI
 app.post('/', async (req,res) => {
     try {
         const prompt = req.body.prompt;
@@ -34,13 +35,18 @@ app.post('/', async (req,res) => {
             presence_penalty: 0,
         });
 
+        // if the status is ok => send text to client via an object
         res.status(200).send({
             bot: response.data.choices[0].text
         })
+
     } catch (error) {
+
+        // else display error for debugging purposes
         console.log(error);
         res.status(500).send({ error })
     }
 })
 
+// always listen for a request sent from client
 app.listen(5001, () => console.log('Server is running on port: http://localhost:5001'));
